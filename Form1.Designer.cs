b01b05e262bc9c568cf256d2a442b1693c2562e4
Chameleon <c-n@bk.ru>
2011-04-10 01:22:22 -0400

    Clean up and nicer look. Support for calculating auth values.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,203 +28,172 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.offsetBox = new System.Windows.Forms.TextBox();
-			this.textBox3 = new System.Windows.Forms.TextBox();
-			this.crypted = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.special = new System.Windows.Forms.TextBox();
-			this.label6 = new System.Windows.Forms.Label();
-			this.specialInput = new System.Windows.Forms.TextBox();
-			this.label7 = new System.Windows.Forms.Label();
-			this.OpcodeForSpecial = new System.Windows.Forms.TextBox();
-			this.label8 = new System.Windows.Forms.Label();
-			this.label9 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(22, 76);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(51, 20);
-			this.textBox1.TabIndex = 0;
-			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-			// 
-			// offsetBox
-			// 
-			this.offsetBox.Location = new System.Drawing.Point(134, 61);
-			this.offsetBox.Name = "offsetBox";
-			this.offsetBox.Size = new System.Drawing.Size(46, 20);
-			this.offsetBox.TabIndex = 1;
-			// 
-			// textBox3
-			// 
-			this.textBox3.Location = new System.Drawing.Point(23, 40);
-			this.textBox3.Name = "textBox3";
-			this.textBox3.Size = new System.Drawing.Size(41, 20);
-			this.textBox3.TabIndex = 2;
-			this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
-			// 
-			// crypted
-			// 
-			this.crypted.Location = new System.Drawing.Point(134, 25);
-			this.crypted.Name = "crypted";
-			this.crypted.Size = new System.Drawing.Size(46, 20);
-			this.crypted.TabIndex = 3;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(131, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(56, 13);
-			this.label1.TabIndex = 4;
-			this.label1.Text = "Converted";
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(131, 45);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(35, 13);
-			this.label2.TabIndex = 5;
-			this.label2.Text = "Offset";
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(19, 60);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(66, 13);
-			this.label3.TabIndex = 6;
-			this.label3.Text = "Opcode dec";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(20, 24);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(65, 13);
-			this.label4.TabIndex = 7;
-			this.label4.Text = "Opcode hex";
-			// 
-			// label5
-			// 
-			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(131, 82);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(42, 13);
-			this.label5.TabIndex = 9;
-			this.label5.Text = "Special";
-			// 
-			// special
-			// 
-			this.special.Location = new System.Drawing.Point(134, 98);
-			this.special.Name = "special";
-			this.special.Size = new System.Drawing.Size(46, 20);
-			this.special.TabIndex = 8;
-			// 
-			// label6
-			// 
-			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(31, 181);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(42, 13);
-			this.label6.TabIndex = 11;
-			this.label6.Text = "Special";
-			// 
-			// specialInput
-			// 
-			this.specialInput.Location = new System.Drawing.Point(26, 210);
-			this.specialInput.Name = "specialInput";
-			this.specialInput.Size = new System.Drawing.Size(47, 20);
-			this.specialInput.TabIndex = 10;
-			this.specialInput.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-			// 
-			// label7
-			// 
-			this.label7.AutoSize = true;
-			this.label7.Location = new System.Drawing.Point(131, 181);
-			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(45, 13);
-			this.label7.TabIndex = 13;
-			this.label7.Text = "Opcode";
-			// 
-			// OpcodeForSpecial
-			// 
-			this.OpcodeForSpecial.Location = new System.Drawing.Point(126, 210);
-			this.OpcodeForSpecial.Name = "OpcodeForSpecial";
-			this.OpcodeForSpecial.Size = new System.Drawing.Size(47, 20);
-			this.OpcodeForSpecial.TabIndex = 12;
-			// 
-			// label8
-			// 
-			this.label8.AutoSize = true;
-			this.label8.Location = new System.Drawing.Point(86, 196);
-			this.label8.Name = "label8";
-			this.label8.Size = new System.Drawing.Size(19, 13);
-			this.label8.TabIndex = 14;
-			this.label8.Text = "=>";
-			// 
-			// label9
-			// 
-			this.label9.AutoSize = true;
-			this.label9.Location = new System.Drawing.Point(91, 47);
-			this.label9.Name = "label9";
-			this.label9.Size = new System.Drawing.Size(19, 13);
-			this.label9.TabIndex = 15;
-			this.label9.Text = "=>";
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(213, 258);
-			this.Controls.Add(this.label9);
-			this.Controls.Add(this.label8);
-			this.Controls.Add(this.label7);
-			this.Controls.Add(this.OpcodeForSpecial);
-			this.Controls.Add(this.label6);
-			this.Controls.Add(this.specialInput);
-			this.Controls.Add(this.label5);
-			this.Controls.Add(this.special);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.crypted);
-			this.Controls.Add(this.textBox3);
-			this.Controls.Add(this.offsetBox);
-			this.Controls.Add(this.textBox1);
-			this.Name = "Form1";
-			this.Text = "Opcode tools";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.opcodeDecBox = new System.Windows.Forms.TextBox();
+            this.offsetBox = new System.Windows.Forms.TextBox();
+            this.opcodeHexBox = new System.Windows.Forms.TextBox();
+            this.cryptedBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.specialBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.authBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // opcodeDecBox
+            // 
+            this.opcodeDecBox.Location = new System.Drawing.Point(28, 100);
+            this.opcodeDecBox.Name = "opcodeDecBox";
+            this.opcodeDecBox.Size = new System.Drawing.Size(51, 20);
+            this.opcodeDecBox.TabIndex = 0;
+            this.opcodeDecBox.TextChanged += new System.EventHandler(this.OpcodeDecBox_TextChanged);
+            // 
+            // offsetBox
+            // 
+            this.offsetBox.Location = new System.Drawing.Point(139, 61);
+            this.offsetBox.Name = "offsetBox";
+            this.offsetBox.Size = new System.Drawing.Size(46, 20);
+            this.offsetBox.TabIndex = 1;
+            this.offsetBox.TextChanged += new System.EventHandler(this.offsetBox_TextChanged);
+            // 
+            // opcodeHexBox
+            // 
+            this.opcodeHexBox.Location = new System.Drawing.Point(29, 64);
+            this.opcodeHexBox.Name = "opcodeHexBox";
+            this.opcodeHexBox.Size = new System.Drawing.Size(50, 20);
+            this.opcodeHexBox.TabIndex = 2;
+            this.opcodeHexBox.TextChanged += new System.EventHandler(this.OpcodeHex_TextChanged);
+            // 
+            // cryptedBox
+            // 
+            this.cryptedBox.Location = new System.Drawing.Point(139, 25);
+            this.cryptedBox.Name = "cryptedBox";
+            this.cryptedBox.Size = new System.Drawing.Size(46, 20);
+            this.cryptedBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Converted";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Offset dec";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Opcode dec";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Opcode hex";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(136, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Special hex";
+            // 
+            // specialBox
+            // 
+            this.specialBox.Location = new System.Drawing.Point(139, 98);
+            this.specialBox.Name = "specialBox";
+            this.specialBox.Size = new System.Drawing.Size(46, 20);
+            this.specialBox.TabIndex = 8;
+            this.specialBox.TextChanged += new System.EventHandler(this.specialBox_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(97, 77);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(25, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "<=>";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(136, 121);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(56, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Auth (dec)";
+            // 
+            // authBox
+            // 
+            this.authBox.Location = new System.Drawing.Point(139, 137);
+            this.authBox.Name = "authBox";
+            this.authBox.Size = new System.Drawing.Size(46, 20);
+            this.authBox.TabIndex = 18;
+            this.authBox.TextChanged += new System.EventHandler(this.authBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 171);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.authBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.specialBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cryptedBox);
+            this.Controls.Add(this.opcodeHexBox);
+            this.Controls.Add(this.offsetBox);
+            this.Controls.Add(this.opcodeDecBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "OpcodeTools 4.0.6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox opcodeDecBox;
 		private System.Windows.Forms.TextBox offsetBox;
-		private System.Windows.Forms.TextBox textBox3;
-		private System.Windows.Forms.TextBox crypted;
+		private System.Windows.Forms.TextBox opcodeHexBox;
+		private System.Windows.Forms.TextBox cryptedBox;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label5;
-		private System.Windows.Forms.TextBox special;
-		private System.Windows.Forms.Label label6;
-		private System.Windows.Forms.TextBox specialInput;
-		private System.Windows.Forms.Label label7;
-		private System.Windows.Forms.TextBox OpcodeForSpecial;
-		private System.Windows.Forms.Label label8;
-		private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox specialBox;
+        private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox authBox;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,47 +11,143 @@ namespace OpcodeTools
 {
 	public partial class Form1 : Form
 	{
+        bool IgnoreTextChanged = false;
+
 		public Form1()
 		{
 			InitializeComponent();
 		}
 
-		private void textBox1_TextChanged(object sender, EventArgs e)
+		private void OpcodeDecBox_TextChanged(object sender, EventArgs e)
 		{
+            if (IgnoreTextChanged)
+                return;
+
 			uint opcode;
-			if(!UInt32.TryParse(textBox1.Text, out opcode))
+			if(!UInt32.TryParse(opcodeDecBox.Text, out opcode))
 				return;
 
-			update(opcode);
+            opcodeHexBox.Text = String.Format("{0:x}", opcode);
+			updateValuesForOpcode(opcode);
 		}
 
-		private void textBox3_TextChanged(object sender, EventArgs e)
+        private void OpcodeHex_TextChanged(object sender, EventArgs e)
+        {
+            try 
+	        {
+                opcodeDecBox.Text = Convert.ToUInt32(opcodeHexBox.Text, 16).ToString();
+	        }
+	        catch (Exception)
+	        {
+	        }
+        }
+        private void CleanValues()
+        {
+            IgnoreTextChanged = true;
+            cryptedBox.Text = specialBox.Text = offsetBox.Text = authBox.Text = "";
+            IgnoreTextChanged = false;
+        }
+
+		void updateValuesForOpcode(uint opcode)
 		{
-			try 
-			{
-				update(Convert.ToUInt32(textBox3.Text, 16));
-			}
-			catch (Exception)
-			{
-			}
-			
+            // start clean
+            CleanValues();
+
+            IgnoreTextChanged = true;
+            if (IsAuthOpcode(opcode))
+            {
+                uint auth = CalcAuthFromOpcode(opcode);
+                authBox.Text = auth.ToString();
+            }
+            else if (IsSpecialOpcode(opcode))
+            {
+                uint specialHandlerNum = CalcSpecialFromOpcode(opcode);
+                specialBox.Text = String.Format("{0:x}", specialHandlerNum);
+            }
+            else if (IsNormalOpcode(opcode))
+            {
+                uint crypt = CalcCryptedFromOpcode(opcode);
+                uint offset = CalcOffsetFromOpcode(opcode);
+			    cryptedBox.Text = crypt.ToString();
+			    offsetBox.Text = offset.ToString();
+            }
+            IgnoreTextChanged = false;
 		}
-		void update(uint a1)
-		{
-			uint offset = (a1 & 0xC | ((a1 & 0x60 | ((a1 & 0x1F00 | (a1 >> 1) & 0x6000) >> 1)) >> 1)) >> 2;
-			crypted.Text = offset.ToString();
-			offset = offset * 4 + 1376;
-			offsetBox.Text = offset.ToString();
-			uint a4 = a1;
-			uint specialHandlerNum = (a4 & 2 | ((a4 & 0x10 | ((a4 & 0x40 | ((a4 & 0x100 | ((a4 & 0xC00 | (a4 >> 2) & 0x1000) >> 1)) >> 1)) >> 1)) >> 2)) >> 1;
-			special.Text = String.Format("{0:x}", specialHandlerNum);
-		}
+
+        private void specialBox_TextChanged(object sender, EventArgs e)
+        {
+            if (IgnoreTextChanged)
+                return;
+            try
+            {
+                SetOpcode(CalcOpcodeFromSpecial(Convert.ToUInt32(specialBox.Text, 16)));
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void offsetBox_TextChanged(object sender, EventArgs e)
+        {
+            if (IgnoreTextChanged)
+                return;
+            try
+            {
+                SetOpcode(CalcOpcodeFromOffset(Convert.ToUInt32(offsetBox.Text)));
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void authBox_TextChanged(object sender, EventArgs e)
+        {
+            if (IgnoreTextChanged)
+                return;
+            try
+            {
+                SetOpcode(CalcOpcodeFromAuth(Convert.ToUInt32(authBox.Text)));
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void SetOpcode(uint opcode)
+        {
+            IgnoreTextChanged = true;
+            opcodeHexBox.Text = String.Format("{0:X}", opcode);
+            opcodeDecBox.Text = opcode.ToString();
+            IgnoreTextChanged = false;
+        }
+
+        #region Verison specific functions. Currently 4.0.6
+
+        private static bool IsAuthOpcode(uint opcode)
+        {
+            return (opcode & 0x3FFD) == 8217;
+        }
+        private static bool IsSpecialOpcode(uint opcode)
+        {
+            return !IsAuthOpcode(opcode) && (opcode & 0xB2AD) == 12;
+        }
+        private static bool IsNormalOpcode(uint opcode)
+        {
+            return !IsSpecialOpcode(opcode) && !IsAuthOpcode(opcode) &&
+                    (opcode & 0x2093) == 8320 && opcode != 60096 && opcode != 44964;
+        }
+
+        private uint CalcCryptedFromOpcode(uint opcode)
+        {
+            uint a1 = opcode;
+            return (a1 & 0xC | ((a1 & 0x60 | ((a1 & 0x1F00 | (a1 >> 1) & 0x6000) >> 1)) >> 1)) >> 2;
+        }
 
 		private uint CalcOffsetFromOpcode(uint opcode)
 		{
-			uint a1 = opcode;
-			uint offset = (a1 & 0xC | ((a1 & 0x60 | ((a1 & 0x1F00 | (a1 >> 1) & 0x6000) >> 1)) >> 1)) >> 2;
-			return (offset * 4) + 1376;
+            uint crypted = CalcCryptedFromOpcode(opcode);
+			return (crypted * 4) + 1376;
 		}
 
 		public uint CalcSpecialFromOpcode(uint opcode)
@@ -60,29 +156,51 @@ namespace OpcodeTools
 			return (a4 & 2 | ((a4 & 0x10 | ((a4 & 0x40 | ((a4 & 0x100 | ((a4 & 0xC00 | (a4 >> 2) & 0x1000) >> 1)) >> 1)) >> 1)) >> 2)) >> 1;
 		}
 
-		private int CalcOpcodeFromSpecial(uint offset)
+        public uint CalcAuthFromOpcode(uint opcode)
+        {
+            uint a1 = opcode;
+            return (a1 & 2 | (a1 >> 12) & 0xC) >> 1;
+        }
+
+        #endregion
+
+        private uint CalcOpcodeFromSpecial(uint offset)
 		{
-			for (uint i = 0; i < 0xFFFF; ++i)
+			for (uint i = 1; i < 0xFFFF; ++i)
 			{
-				if ((i & 0xB2AD) == 12 )
+				if (IsSpecialOpcode(i))
 				{
 					if (CalcSpecialFromOpcode(i) == offset)
-						return (int)i;
+						return i;
 				}
 			}
-			return -1;
+			return 0;
 		}
 
-		private void textBox2_TextChanged(object sender, EventArgs e)
+		private uint CalcOpcodeFromOffset(uint offset)
 		{
-			try
+			for (uint i = 1; i < 0xFFFF; ++i)
 			{
-				OpcodeForSpecial.Text = String.Format("{0:x}",
-					CalcOpcodeFromSpecial(Convert.ToUInt32(specialInput.Text, 16)));
+				if (IsNormalOpcode(i))
+				{
+					if (CalcOffsetFromOpcode(i) == offset)
+						return i;
+				}
 			}
-			catch (Exception)
+			return 0;
+		}
+
+		private uint CalcOpcodeFromAuth(uint auth)
+		{
+			for (uint i = 1; i < 0xFFFF; ++i)
 			{
+                if (IsAuthOpcode(i) && 
+					CalcAuthFromOpcode(i) == auth)
+				{
+						return i;
+				}
 			}
+			return 0;
 		}
 	}
 }
